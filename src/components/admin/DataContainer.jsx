ae64617b9b8bf831ac64e29add7096645b468d4f
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2024-02-18 21:10:23 +0200

    admin 002

--- src/components/admin/DataContainer.jsx
+++ src/components/admin/DataContainer.jsx
@@ -2,11 +2,12 @@ import React from "react";
 
 const DataContainer = (props) => {
   return (
-    <div className="done">
+    <div className={`data-container ${props.className}`}>
       <p className="title">
         <span>{props.title}</span>
         <img src={props.image} alt={props.alt} />
       </p>
+      <span className="count">{props.count}</span>
     </div>
   );
 };
--- src/components/admin/Main.jsx
+++ src/components/admin/Main.jsx
@@ -1,13 +1,35 @@
 import React from "react";
 import DataContainer from "./DataContainer";
 import doneIcon from "../../icons/Checks.svg";
-
+import queuedIcon from "../../icons/Bell.svg";
+import canceledIcon from "../../icons/X.svg";
+import "./scss/Main.css";
 const Main = () => {
   return (
-    <div className="admin-page">
+    <div className="admin-page-main">
       <div className="appointments">
         <p className="title">Appointments</p>
-        <DataContainer image={doneIcon} alt="done-icon" title="Done" />
+        <DataContainer
+          image={doneIcon}
+          alt="done-icon"
+          title="Done"
+          count={29}
+          className="done"
+        />
+        <DataContainer
+          image={queuedIcon}
+          alt="done-icon"
+          title="Queued"
+          count={10}
+          className="queued"
+        />
+        <DataContainer
+          image={canceledIcon}
+          alt="done-icon"
+          title="Canceled"
+          count={4}
+          className="canceled"
+        />
       </div>
     </div>
   );
