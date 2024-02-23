eafda986456fff7ca4c9389a8d7bef5799a2b62f
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2024-02-23 20:47:22 +0200

    Update ProtectedRoutes.jsx

--- src/routes/ProtectedRoutes.jsx
+++ src/routes/ProtectedRoutes.jsx
@@ -1,7 +1,6 @@
-import React, { useContext } from "react";
+import React from "react";
 import { Navigate, useLocation, Outlet } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
-import UnAuthorized from "./UnAuthorized";
 
 const ProtectedRoutes = ({ allowedRoles }) => {
   const isAuth = useAuth();
