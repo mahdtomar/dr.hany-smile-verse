5a46b41bb7709dddc5a078e33dbe10a9dc75b814
saramohamed124 <saramohamaden1234@gmail.com>
2024-02-28 22:42:32 +0200

    faqquestions

--- src/components/faq/components/Faqquestions.jsx
+++ src/components/faq/components/Faqquestions.jsx
@@ -1,10 +1,30 @@
+// import plus_icon from "./assets/icons/plus_icon.svg";
+// import minus_icon from "./assets/icons/minus_icon.svg";
+
 function Faqquestions() {
   return (
-    <div className="container">
-      <div className="content">
+    <div className="faq-questions container-faq d-flex justify-between">
+      <div className="faq-content">
+        <span>Support</span>
         <h1>FAQs</h1>
+        <p>
+          Every thing you need to know about us. Can't find the answer you're
+          looking for ? please write your questions in feedback questions .
+        </p>
       </div>
-      <></>
+      <ul>
+        <li>
+          <details open>
+            <summary className="d-flex">
+              How will we conduct IEP and 504 meetings?
+            </summary>
+            <div>
+              <p>All meetings will be held remotely.</p>
+            </div>
+          </details>{" "}
+        </li>
+        <li></li>
+      </ul>
     </div>
   );
 }
