69aa56b798bbd14f9cdfd0739e79520605cab179
Omar Mahdy <72965518+mahdtomar@users.noreply.github.com>
2024-03-25 14:57:13 +0200

    Update AlignmentTeethController.jsx

--- src/components/admin/dashboard/components/services/AlignmentTeethController.jsx
+++ src/components/admin/dashboard/components/services/AlignmentTeethController.jsx
@@ -1,9 +1,36 @@
 import React from 'react'
-
+import { AlignmentTeethControllerData } from '../../../../../Data'
+const data = AlignmentTeethControllerData.sections
+console.log(data)
+const Controles = ({ ref }) => {
+  return <>
+    <button >Edit</button></>
+}
 const AlignmentTeethController = () => {
   return (
-    <div>AlignmentTeethController</div>
+    <div>
+      {data.map((section) => {
+        return <section>
+          <div className="flex-2">
+            <div className="text">
+
+              {section.subTitle ? <>
+                <span>{section.subTitle}</span>
+                <input type="text" disabled />
+                <Controles ref={""} />
+              </> : ""}
+
+              {section.title ? <h1>{section.title}</h1> : ""}
+              {section.description ? section.description.map((e) => { return <p>{e}</p> }) : ""}
+            </div>
+            {section.img ? <img src={section.img} alt='' /> : ""}
+          </div>
+        </section>
+      })}
+    </div>
   )
 }
 
 export default AlignmentTeethController
+
+
